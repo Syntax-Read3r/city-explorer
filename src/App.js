2cd4cd3ea8d3f17d09633e9eb856294cc0708dcb
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-07-19 16:52:58 +0100

    add location component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Location from './Location/Location'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      location: {},
+      longitude: 0,
+      latitude: 0
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchQuery: event.target.value,
+    });
+    console.log(this.state.searchQuery);
+  };
+
+  getLocation = async () => {
+    console.log(process.env.REACT_APP_API_KEY)
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.searchQuery}&format=json`;
+    const res = await axios.get(API);
+     this.setState({ 
+      location: res.data[0],
+      longitude: res.data[0].lon,
+      latitude: res.data[0].lat,
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <>
+        <input
+          onChange={this.handleChange}
+          placeholder="Serch for a city"
+        ></input>
+        <button onClick={this.getLocation}>Explore!</button>
+        {/* conditionally show the name of the place */}
+        {this.state.location.display_name && (
+          <Location location={this.state.location} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Location/Location.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class Location extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Location:</td>
+                            <td>{this.props.location.display_name}</td>
+                        </tr>
+                        <tr>
+                            <td>Latitude:</td>
+                            <td>{this.props.location.lat}</td>
+                        </tr>
+                        <tr>
+                            <td>Longitude:</td>
+                            <td>{this.props.location.lat}</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        );
+    }
+}
